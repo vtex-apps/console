77b3e80e14b25433b51afce6f0e5469bdf747a55
luizerasmo <luiz.moreira@vtex.com.br>
2018-10-19 01:33:23 -0300

    Add layout query and Metrics component

--- react/components/Metrics.tsx
+++ react/components/Metrics.tsx
@@ -1,17 +1,78 @@
 import moment, { Moment } from 'moment'
 import React, { Component } from 'react'
+import { Query } from 'react-apollo'
 import { EmptyState } from 'vtex.styleguide'
+
+import { Render } from './render'
+
+import layoutQuery from '../graphql/layout.graphql'
 
 interface Props {
   controllers: Controllers
 }
 
+const getAppName = (controllers: Controllers) => {
+  const {
+    chosenAppName,
+    chosenMajor,
+    chosenMinor,
+    chosenPatch,
+  } = controllers
+  return chosenAppName ?
+         `${chosenAppName}@${chosenMajor}.${chosenMinor}.${chosenPatch}` :
+         null
+}
+
 export default class Metrics extends Component<Props> {
-  public render = () => (
-    <EmptyState title="Coming Soon">
-      <p>
-        Formula VTEX should last LONGER !!!.
-      </p>
-    </EmptyState>
-  )
+  constructor(props: any) {
+    super(props)
+  }
+
+  public render = () => {
+    const { controllers } = this.props
+
+    const appName = getAppName(controllers)
+
+    return (
+      appName ?
+      (
+        <Query query={layoutQuery} ssr={false} variables={{appName}}>
+            {({loading, data: {layout}}) => (
+              !loading && layout && Array.isArray(layout) ? (
+                <div className="flex flex-wrap">
+                    {
+                      layout.map(({spec, specLocator}) => {
+                        const specJSON = JSON.parse(spec)
+                        const {
+                          storedash: {
+                            name,
+                            params
+                          }
+                        } = specJSON
+                        return (
+                          <div className="w-45 pa3 mr2">
+                              <Render
+                                appId={appName}
+                                name={name}
+                                params={params}
+                                spec={specJSON}
+                              />
+                          </div>
+                        )
+                      })
+                    }
+                </div>
+              ) : null
+            )
+            }
+        </Query>
+      ) : (
+        <EmptyState title="Please select an app">
+            <p>
+                Please select an app to see its corresponding metrics
+            </p>
+        </EmptyState>
+      )
+    )
+  }
 }
