ee5e2f4d5c268073a584e8879e83c1728b1d8298
germanohn <germano.huning@vtex.com.br>
2019-02-04 15:39:15 -0200

    Put mapTime and stepModifierOptions as global variables

--- react/components/Metrics/Controllers/TimeControllers/DateRange/RangeStepPicker.tsx
+++ react/components/Metrics/Controllers/TimeControllers/DateRange/RangeStepPicker.tsx
@@ -3,8 +3,9 @@ import { Dropdown, NumericStepper } from 'vtex.styleguide'
 
 import { dropdownOptions } from '../../../../../common/utils'
 import { EventWithValue } from '../../../../../typings/events'
-import { RangeStep } from './typings'
 
+
+type RangeStep = 'Full' | 'Seconds' | 'Minutes' | 'Hours' | 'Days' | 'Months' | 'Years'
 
 interface Props {
   rangeStep: string
@@ -16,6 +17,25 @@ interface State {
   stepModifier: RangeStep
 }
 
+
+const mapTime = {
+  'Days': 'd',
+  'Hours': 'h',
+  'Minutes': 'm',
+  'Months': 'M',
+  'Seconds': 's',
+  'Years': 'y',
+}
+
+const stepModifierOptions: string[] = [
+  'Full',
+  'Seconds',
+  'Minutes',
+  'Hours',
+  'Days',
+  'Months',
+  'Years',
+]
 
 export default class RangeStepPicker extends Component<Props, State> {
   constructor(props: Props) {
@@ -36,29 +56,12 @@ export default class RangeStepPicker extends Component<Props, State> {
     if (this.state.stepModifier === 'Full') {
       this.props.handleRangeStep('')
     } else {
-      const mapTime = {
-        'Days': 'd',
-        'Hours': 'h',
-        'Minutes': 'm',
-        'Months': 'M',
-        'Seconds': 's',
-        'Years': 'y',
-      }
       const rangeStep = this.state.stepSize + mapTime[this.state.stepModifier]
       this.props.handleRangeStep(rangeStep)
     }
   }
 
   public render = () => {
-    const stepModifierOptions: string[] = [
-      'Full',
-      'Seconds',
-      'Minutes',
-      'Hours',
-      'Days',
-      'Months',
-      'Years',
-    ]
 
     return (
       <div className="flex flex-colunm items-end pa4 mh0">
