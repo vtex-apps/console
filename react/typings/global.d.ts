44e3b2a0ac777bae57504a33c6a6df627655c199
germanohn <germano.huning@vtex.com.br>
2019-02-04 17:56:27 -0200

    Move vtex.styleguide module to a separate file

--- react/typings/global.d.ts
+++ react/typings/global.d.ts
@@ -40,29 +40,3 @@ type SetTimeControllers = (timeControllers: TimeController) => void
 
 type SetEnvControllers = (envControllers: EnvController) => void
 
-declare module 'vtex.styleguide' {
-  import { ReactElement } from 'react'
-
-  const Badge: ReactElement
-  const Button: ReactElement
-  const Checkbox: ReactElement
-  const DatePicker: ReactElement
-  const Dropdown: ReactElement
-  const EmptyState: ReactElement
-  const IconCaretRight: ReactElement
-  const Input: ReactElement
-  const Layout: ReactElement
-  const NumericStepper: ReactElement
-  const Tab: ReactElement
-  const Tabs: ReactElement
-  const Textarea: ReactElement
-  const Radio: ReactElement
-  const RadioGroup: ReactElement
-  const Spinner: ReactElement
-  const PageHeader: ReactElement
-  const PageBlock: ReactElement
-
-  export { Button, Dropdown, DatePicker, EmptyState, Input, Layout, NumericStepper, Tab, Tabs, Radio, RadioGroup, Spinner, PageBlock, PageHeader }
-}
-
-
