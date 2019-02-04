45e13bcc86a86a23a54b2c59a18ece454dfd32a9
germanohn <germano.huning@vtex.com.br>
2019-02-04 17:49:20 -0200

    Change handleValue by FormattedNumber

--- react/components/Metrics/DataAnalysis/CustomElements/CustomTooltip.tsx
+++ react/components/Metrics/DataAnalysis/CustomElements/CustomTooltip.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { InjectedIntlProps, injectIntl } from 'react-intl'
+import { FormattedNumber, InjectedIntlProps, injectIntl  } from 'react-intl'
 import { PageBlock } from 'vtex.styleguide'
 
 
@@ -12,16 +12,6 @@ interface Props extends InjectedIntlProps {
 
 
 const CustomTooltip = (props: Props) => {
-  const handleLabel = (label: string) => {
-    // After it will treat intl
-    return label
-  }
-
-  const handleValue = (value: number) => {
-    const { intl } = props
-    return intl.formatNumber(value)
-  }
-
   const { active } = props
 
   if (active) {
@@ -35,37 +25,37 @@ const CustomTooltip = (props: Props) => {
           </div>
           <div className="flex justify-between" >
             <div className="pa2" style={{ color: payload[0].color }} >
-              {`${handleLabel(payload[0].dataKey)}:  `}
+              {`${payload[0].dataKey}:  `}
             </div>
             <div className="pa2" >
-              {handleValue(payload[0].value)}
+              <FormattedNumber value={payload[0].value} />
             </div>
           </div>
 
           <div className="flex justify-between" >
             <div className="pa2" style={{ color: payload[1].color }} >
-              {`${handleLabel(payload[1].dataKey)}:  `}
+              {`${payload[1].dataKey}:  `}
             </div>
             <div className="pa2" >
-              {handleValue(payload[1].value)}
+              <FormattedNumber value={payload[1].value} />
             </div>
           </div>
 
-          <div className="flex justify-around" >
+          <div className="flex justify-between" >
             <div className="pa2" style={{ color: payload[2].color }} >
-              {`${handleLabel(payload[2].dataKey)}:  `}
+              {`${payload[2].dataKey}:  `}
             </div>
             <div className="pa2" >
-              {handleValue(payload[2].value)}
+              <FormattedNumber value={payload[2].value} />
             </div>
           </div>
 
           <div className="flex justify-between" >
             <div className="pa2" style={{ color: payload[3].color }} >
-              {`${handleLabel(payload[3].dataKey)}:  `}
+              {`${payload[3].dataKey}:  `}
             </div>
             <div className="pa2" >
-              {handleValue(payload[3].value)}
+              <FormattedNumber value={payload[3].value} />
             </div>
           </div>
         </div>
