5c0ca1246332a34c498cf1fec02e43bd2d562ee0
germanohn <germano.huning@vtex.com.br>
2019-02-11 17:30:12 -0200

    Remove unnecessary functions in utils

--- react/components/Metrics/Controllers/TimeControllers/Absolute/utils.ts
+++ react/components/Metrics/Controllers/TimeControllers/Absolute/utils.ts
@@ -12,16 +12,12 @@ export const mapTime = {
   'Years': 'y',
 }
 
-const setStepModifierOptions = () => {
-  return [
-    { value: 'Full', label: 'console.rangeStepPicker.stepModifier.full' },
-    { value: 'Seconds', label: 'console.rangeStepPicker.stepModifier.seconds' },
-    { value: 'Minutes', label: 'console.rangeStepPicker.stepModifier.minutes' },
-    { value: 'Hours', label: 'console.rangeStepPicker.stepModifier.hours' },
-    { value: 'Days', label: 'console.rangeStepPicker.stepModifier.days' },
-    { value: 'Months', label: 'console.rangeStepPicker.stepModifier.months' },
-    { value: 'Years', label: 'console.rangeStepPicker.stepModifier.years' },
-  ]
-}
-
-export const stepModifierOptions: Option[] = setStepModifierOptions()
+export const stepModifierOptions: Option[] = [
+  { value: 'Full', label: 'console.rangeStepPicker.stepModifier.full' },
+  { value: 'Seconds', label: 'console.rangeStepPicker.stepModifier.seconds' },
+  { value: 'Minutes', label: 'console.rangeStepPicker.stepModifier.minutes' },
+  { value: 'Hours', label: 'console.rangeStepPicker.stepModifier.hours' },
+  { value: 'Days', label: 'console.rangeStepPicker.stepModifier.days' },
+  { value: 'Months', label: 'console.rangeStepPicker.stepModifier.months' },
+  { value: 'Years', label: 'console.rangeStepPicker.stepModifier.years' },
+]
--- react/components/Metrics/Controllers/TimeControllers/utils.ts
+++ react/components/Metrics/Controllers/TimeControllers/utils.ts
@@ -1,8 +1,7 @@
-const setTimeControllerOptions = () => {
-  return [
-    { value: 'Absolute', label: 'console.time.controller.absolute' },
-    { value: 'Relative', label: 'console.time.controller.relative' },
-  ]
-}
+import { Option } from '../../../../common/utils'
 
-export const timeControllerOptions = setTimeControllerOptions()
+
+export const timeControllerOptions: Option[] = [
+  { value: 'Absolute', label: 'console.time.controller.absolute' },
+  { value: 'Relative', label: 'console.time.controller.relative' },
+]
